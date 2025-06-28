7f85668aa7c0bae47acfd3528052bb981904d901
AmOglock <amoglock@gmail.com>
2025-06-28 21:22:26 +0300

    update schemas

--- app/inventory/schemas.py
+++ app/inventory/schemas.py
@@ -1,14 +1,12 @@
 from enum import Enum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sqlmodel import SQLModel
 
 
-class ConsumeRequest(BaseModel):
-    item_id: int
-    quantity: int
-    battle_id: str
-    user_id: int
+class SuccessResponse(BaseModel):
+    success: bool = True
+    detail: str
 
 
 class ItemKind(str, Enum):
@@ -34,14 +32,10 @@ class ItemCreate(SQLModel):
 
 
 class ItemToInventory(BaseModel):
-    item_id: int
-    amount: int
-
-
-class ItemToInventoryByUserId(BaseModel):
-    item_id: int
-    amount: int
-    user_id: int
+    """Модель добавления предмета в инвентарь игрока"""
+    user_id: int = Field(description="ID игрока, в чей инвентарь добавляется предмет")
+    item_id: int = Field(description="ID предмета для добавления в инвентарь")
+    amount: int = Field(gt=0, description="Количество добавляемых предметов")
 
 
 class InventoryItemResponse(SQLModel):
@@ -58,3 +52,9 @@ class InventoryResponse(SQLModel):
 class UserInfo(SQLModel):
     user_id: int
     role: str
+
+class UseItem(BaseModel):
+    """Модель использования предмета (списания предмета из инвентаря)"""
+    item_id: int = Field(gt=0, description="ID использованного предмета")
+    user_id: int = Field(gt=0, description="ID пользователя, использовавшего предмет")
+    amount: int = Field(gt=0, default=1, description="Количество элементов на использование")
