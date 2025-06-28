8c67f35013cf92f28036241484ad4783c5d9eb4e
AmOglock <amoglock@gmail.com>
2025-06-28 21:21:12 +0300

    fix quotes

--- app/api/items.py
+++ app/api/items.py
@@ -1,24 +1,24 @@
 import logging
 from typing import Annotated
 
-from fastapi import APIRouter, HTTPException, status, Path
+from fastapi import APIRouter, Path
 from fastapi.params import Depends
 
-from app.api.responses import ITEM_NOT_FOUND_RESPONSE, SERVICE_ERROR, UNEXPECTED_ERROR
+from app.api.responses import NOT_FOUND_RESPONSE, SERVICE_ERROR, UNEXPECTED_ERROR
 from app.inventory.schemas import ItemCreate, ItemResponse
 from app.services.item_service import ItemService
 
 logger = logging.getLogger(__name__)
 
 router = APIRouter(
-    prefix="/items",
-    tags=["items"],
+    prefix='/items',
+    tags=['items'],
     responses={**SERVICE_ERROR, **UNEXPECTED_ERROR}
 )
 
 
 @router.get(
-    "/",
+    '/',
     response_model=list[ItemResponse],
     summary="Получить список всех предметов",
     description="Возвращает список всех предметов в игре",
@@ -31,7 +31,7 @@ async def get_items(
 
 
 @router.post(
-    "/create",
+    '/create',
     response_model=ItemResponse,
     summary="Создать новый предмет. Доступно только администратору",
     description="Возвращает созданный предмет", )
@@ -43,9 +43,9 @@ async def create_item(
 
 
 @router.get(
-    "/{item_id}",
+    '/{item_id}',
     response_model=ItemResponse,
-    responses=ITEM_NOT_FOUND_RESPONSE,
+    responses=NOT_FOUND_RESPONSE,
     summary="Получить предмет по его ID",
     description="Возвращает предмет по его уникальному идентификатору",
 )
@@ -54,9 +54,4 @@ async def get_item_by_id(
         item_id: int = Path(..., gt=0, description="ID предмета (должен быть больше 0)"),
 ):
     item = await item_service.get_item(item_id)
-    if item is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Item with ID {item_id} not found"
-        )
     return item
